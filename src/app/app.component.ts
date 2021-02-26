a3d52c1e4a3a35f15e61c3fb16f4ee5a1b154cbc
bclayton21 <brandon@architectmindframe.com>
2021-02-25 20:34:08 -0800

    Uncommenting api call within service to use endpoint and added toggle for data source

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,19 +12,53 @@ import { CarouselDataService } from '../app/services/carousel-data.service';
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
 	public slides: Array<SlideInterface> = [];
+	shopping_data_source: any;
+	cars_data_source: any;
+	data_source: 'shopping' | 'cars' = 'shopping';
+
 
 	constructor(private carousel_service: CarouselDataService){}
 
 	ngOnInit(): void {
 
-		// this.carousel_service.getCarouselImages().subscribe((data) => {
-		// 	console.log('App', data);
-		// });
+		this.getData();
 
-		const result = this.carousel_service.getCarouselImages();
+	}
 
-		this.slides = result.data;
+	toggleDataSource(){
+
+		if(this.data_source === 'shopping'){
+			this.data_source = 'cars';
+		} else if(this.data_source === 'cars'){
+			this.data_source = 'shopping';
+		}
+
+		this.getData();
+
+	}
+
+	getData(){
+
+		if(this.data_source === 'shopping'){
+
+			this.shopping_data_source = this.carousel_service.getCarouselImages();
+			
+			this.shopping_data_source.subscribe((result: any) => {
+				this.slides = result.data;
+			});
+			
+		} else if(this.data_source === 'cars'){
+
+			this.cars_data_source = this.carousel_service.getCarCarouselImages();
+			
+			this.cars_data_source.subscribe((result: any) => {
+				this.slides = result.data;
+			});
+
+		}
+		
 
 	}
 }
